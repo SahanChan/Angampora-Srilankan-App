8c684c1edd5b5f419a6534200c438cd2ce125ebd
SahanChan <xnscb8.chandrabahu@gmail.com>
2019-07-28 17:48:26 +0530

    test

--- Files/src/AngamApp.py
+++ Files/src/AngamApp.py
@@ -3,6 +3,7 @@ from kivy.uix.label import Label
 
 class AngamApp(App):
     def build(self):
+        print("Hello World")
         return Label(text='Angampora\nIn Development',font_size='20sp')
 
 if __name__=='__main__':
